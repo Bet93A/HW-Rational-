cdb11db7276fa31ef4c63f34d0446b7d63bc414d
Anastasiia Kolpakova <betik937@gmail.com>
2023-04-02 18:03:45 +0300

    Add Test Minus

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -8,21 +8,25 @@ public class Tests {
         assertEquals("Standard constructor returns wrong numerator", 0, standard.getNumerator());
         assertEquals("Standard constructor returns wrong denominator", 1, standard.getDenominator());
     }
+
     @Test(expected = ArithmeticException.class)
     public void testDenominator0() {
         new Rational(5, 0);
     }
+
     @Test
     public void testSimplifyMinuses() {
         assertEquals("minus is not in the numerator",new Rational(1,-5),new Rational(-1,5));
         assertEquals("minus + minus != plus", new Rational(-1,-5),new Rational(1,5));
         assertEquals("numerator = -0", new Rational(-0,1),new Rational());
     }
+
     @Test
     public void testReduce() {
         Rational Reduce =new Rational(-15,-20);
         assertEquals("The fraction is not reduced", Reduce, new Rational(3,4));
     }
+
     @Test
     public void testPlus() {
         Rational Positive = new Rational(1,2);
@@ -34,6 +38,20 @@ public class Tests {
         assertEquals("Negative + Standart",Negative.plus(Standart), Negative);
         assertEquals("Standart + Standart",Standart.plus(Standart), Standart);
     }
+
+    @Test
+    public void testMinus() {
+        Rational Positive = new Rational(1,2);
+        Rational Negative = new Rational(1,-2);
+        Rational Standart = new Rational();
+        assertEquals("Positive - Positive",Positive.minus(Positive), Standart);
+        assertEquals("Negative - Positive",Negative.minus(Positive), new Rational(-1,1));
+        assertEquals("Positive - Negative",Positive.minus(Negative), new Rational(1,1));
+        assertEquals("Negative - Negative",Negative.minus(Negative), Standart);
+        assertEquals("Negative - Standart",Negative.minus(Standart), Negative);
+        assertEquals("Standart - Standart",Standart.minus(Standart), Standart);
+    }
+
     @Test
     public void testMultiply() {
         Rational Positive = new Rational(1,2);
@@ -46,6 +64,7 @@ public class Tests {
         assertEquals("Negative * Standart",Negative.multiply(Standart), Standart);
         assertEquals("Standart * Standart",Standart.multiply(Standart), Standart);
     }
+
     @Test
     public void testDividePositive() {
         Rational Positive = new Rational(2,3);
@@ -57,6 +76,7 @@ public class Tests {
         assertEquals("Positive / Negative",Positive.divide(Negative), new Rational(-2,1));
         assertEquals("Standart / Positve",Standart.divide(Positive), Standart);
     }
+
     @Test(expected = ArithmeticException.class)
     public void testDivideNegative() {
         Rational Positive = new Rational(2,3);
